97a9d68580497e2bbef30774a83180c022a81280
jiangnancyr <1554349628@qq.com>
2020-09-09 14:32:22 +0800

    加上nosetest

--- test_main.py
+++ test_main.py
@@ -10,9 +10,6 @@ import time
 from Env.env import agentParams, resMap
 import matplotlib.pyplot as plt
 
-from swarm_control_distance.agentsInit import AgentInit
-from swarm_control_distance.getData.dataAnalysis import DataAnalysis
-
 
 class Main:
 
@@ -295,7 +292,7 @@ class Main:
         dataMap['entropy_list'] = entropy_list
         return dataMap
 
-class testClass:
+class test_base(object):
     def __init__(self):
         self.main = Main('mainConfig')
         self.mainConfig = self.main.mainConfig
@@ -303,145 +300,5 @@ class testClass:
         # 从algorithms中得到
         self.algorithmParams = algorithmParams
 
-    def test_k1(self):
-
-        simulation = self.mainConfig['simulation']
-        fileName = './resData/' + simulation['algorithmName'] + \
-                                 '_' + str(self.agentConfig['N']) + \
-                                 '_' + str(self.agentConfig['L']) + \
-                                 '_' + str(self.agentConfig['R']) + \
-                                 '_' + str(self.agentConfig['V']) + \
-                                 '_k1' + \
-                                 '.xlsx'
-        print(fileName)
-
-        row = 0
-        for k1 in range(-50, 0, 2):
-            self.algorithmParams['syncControlDistance']['k1'] = k1
-            dataMap = self.main.run(row)
-            while True:
-                try:
-                    we = writeExcel(fileName=fileName)
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['syncRate'], 'syncRate')
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['entropy'], 'entropy')
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['localSyncRate'], 'localSyncRate')
-                    we.saveExcel()
-                    break
-                except PermissionError:
-                    time.sleep(1)
-
-    def test_noise(self):
-
-        simulation = self.mainConfig['simulation']
-        fileName = './resData/' + simulation['algorithmName'] + \
-                                 '_' + str(self.agentConfig['N']) + \
-                                 '_' + str(self.agentConfig['L']) + \
-                                 '_' + str(self.agentConfig['R']) + \
-                                 '_' + str(self.agentConfig['V']) + \
-                                 '_noise' + \
-                                 '.xlsx'
-        print(fileName)
-
-        row = 0
-        for k1 in range(-20, 20, 2):
-            self.algorithmParams['syncControlDistance']['k1'] = k1
-            dataMap = self.main.run(row)
-            while True:
-                try:
-                    we = writeExcel(fileName=fileName)
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['syncRate'], 'syncRate')
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['entropy'], 'entropy')
-                    we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['localSyncRate'], 'localSyncRate')
-                    we.saveExcel()
-                    break
-                except PermissionError:
-                    time.sleep(1)
-
-    def catch_figure(self):
-        row = 0
-        self.main.run(row)
-        while True:
-            plt.pause(0.1)
-
-    def test_noises(self):
-        simulation = self.mainConfig['simulation']
-        k1 = self.algorithmParams['syncControlDistance']['k1']
-        fileName = './resData/' + simulation['algorithmName'] + \
-                                 '_' + str(self.agentConfig['N']) + \
-                                 '_' + str(self.agentConfig['L']) + \
-                                 '_' + str(self.agentConfig['R']) + \
-                                 '_' + str(self.agentConfig['V']) + \
-                                 '_' + str(k1) + \
-                                 '_noise' + \
-                                 '.xlsx'
-        print(fileName)
-        row = 0
-        for k1 in range(0, 30):
-            agentParams['noise'] = k1*0.05
-            dataMap = self.main.run_noise(row)
-            while True:
-                try:
-                    we = writeExcel(fileName=fileName)
-                    we.writeRows(int(k1) + 1, dataMap['syncRate_list'], 'syncRate_list')
-                    we.writeRows(int(k1) + 1, dataMap['entropy_list'], 'entropy_list')
-                    # we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['localSyncRate'], 'localSyncRate')
-                    we.saveExcel()
-                    break
-                except PermissionError:
-                    time.sleep(1)
-
-    def test_time_entropy(self):
-        simulation = self.mainConfig['simulation']
-        fileName = './resData/' + simulation['algorithmName'] + \
-                                 '_' + str(self.agentConfig['N']) + \
-                                 '_' + str(self.agentConfig['L']) + \
-                                 '_' + str(self.agentConfig['R']) + \
-                                 '_' + str(self.agentConfig['V']) + \
-                                 '_time_entropy' + \
-                                 '.xlsx'
-        print(fileName)
-        row = 0
-        for k1 in range(0, 100):
-            dataMap = self.main.run_time(row)
-            while True:
-                try:
-                    we = writeExcel(fileName=fileName)
-                    we.writeCell(row + 1, k1 + 1, dataMap['entropy'], 'entropy')
-                    we.writeCell(row + 1, k1 + 1, dataMap['count'], 'count')
-                    # we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['localSyncRate'], 'localSyncRate')
-                    we.saveExcel()
-                    break
-                except PermissionError:
-                    time.sleep(1)
-
-    def test_step(self):
-        simulation = self.mainConfig['simulation']
-        k1 = self.algorithmParams['syncControlDistance']['k1']
-        fileName = './resData/' + simulation['algorithmName'] + \
-                                 '_' + str(self.agentConfig['N']) + \
-                                 '_' + str(self.agentConfig['L']) + \
-                                 '_' + str(self.agentConfig['R']) + \
-                                 '_' + str(self.agentConfig['V']) + \
-                                 '_' + str(k1) + \
-                                 '_step' + \
-                                 '.xlsx'
-        print(fileName)
-        for row in range(0, 100):
-            dataMap = self.main.run_step(row)
-            while True:
-                try:
-                    we = writeExcel(fileName=fileName)
-                    we.writeRows(int(k1) + 1, dataMap['syncRate_list'], 'syncRate_list')
-                    we.writeRows(int(k1) + 1, dataMap['entropy_list'], 'entropy_list')
-                    # we.writeCell(row + 1, int((k1 + 100) / 2) + 1, dataMap['localSyncRate'], 'localSyncRate')
-                    we.saveExcel()
-                    break
-                except PermissionError:
-                    time.sleep(1)
-
-if __name__ == '__main__':
-    # main = Main('mainConfig')
-    # main.run(0)
-    test = testClass()
-    test.test_noises()
-
+if __name__=='__main__':
+    pass
